0adba1945b76a42728b56e985247f7a769732dc7
David Tuite <dtuite@gmail.com>
2013-10-27 20:13:50 +0000

    Improve styling and UX

--- javascripts/app.js
+++ javascripts/app.js
@@ -30,32 +30,70 @@ require([
 
     var $currentPageviews = $('#current-pageviews');
     var $currentConversions = $('#current-conversions');
+    var $resultCells = $('tbody tr td:last-child');
+    var $errorArea = $('.alert');
+    var errorTimer;
 
-    var results = function() {
+    var isNumber = function(n) {
+      return !isNaN(parseFloat(n)) && isFinite(n);
+    };
+
+    var update = function() {
+      var pageviews = $currentPageviews.val(),
+          conversions = $currentConversions.val();
+
+      $resultCells.empty();
+      $errorArea.empty().addClass('invisible');
+      $currentPageviews.closest('.form-group').removeClass('has-error');
+      $currentConversions.closest('.form-group').removeClass('has-error');
+      clearTimeout(errorTimer);
+      
+
+      if (pageviews === '' || !isNumber(pageviews)) {
+        $currentPageviews.closest('.form-group').addClass('has-error');
+      } else if (conversions === '' || !isNumber(conversions)) {
+        $currentConversions.closest('.form-group').addClass('has-error');
+      // It makes no sense to have more conversions than pageviews.
+      } else if (parseFloat(conversions, 10) > parseFloat(pageviews, 10)) {
+        // If I show the user an error message straight away when they change 
+        // the pageviews input then they end up seeing errors when all they're 
+        // trying to do is tab into the conversion box to change that. 
+        // Better to delay and only show the error if they appear stuck.
+        errorTimer = setTimeout(function() {
+          $errorArea.text('Oops! You can\'t have more conversions than pageviews!').removeClass('invisible');
+        }, 5000);
+      } else {
+        populateResultsTable(results(pageviews, conversions));
+      }
+    }
+
+    var results = function(pageviews, conversions) {
       return [0.01, 0.02, 0.03, 0.05, 0.10].map(function(mde) {
         return new Calculator({
-          currentPageviews: $currentPageviews.val(),
-          currentConversions: $currentConversions.val(),
+          currentPageviews: pageviews,
+          currentConversions: conversions,
           mde: mde
         }).daysNeeded();
       });
     };
 
     var populateResultsTable = function(numbers) {
-      $('tbody tr td:last-child').each(function(index, el) {
+      $resultCells.each(function(index, el) {
         el.innerHTML = (Math.round(numbers.shift() * 100) / 100) + ' days';
       });
     };
 
-    $(inputSelectors).on('change', function(e) {
-      populateResultsTable(results());
-    });
+    var emptyResultsTable = function() { 
+      $resultCells.empty();
+    };
+
+    $(inputSelectors).on('keyup', update);
 
     $('form').on('submit', function(e) {
       e.preventDefault();
-      populateResultsTable(results());
+      update();
     });
 
-    populateResultsTable(results());
+    update();
   });
 });
